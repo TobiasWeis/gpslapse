ec16f8a380a0832cf8efddd3776d45c55315fc97
TobiasWeis <weis.tobi@googlemail.com>
2016-05-28 22:43:48 +0200

    OSM maps

--- balticmap.py
+++ balticmap.py
@@ -10,6 +10,78 @@ from common import DBInterface
 import pickle
 from gauge import *
 import math
+from utils import *
+from smopy import smopy
+import os
+import datetime
+
+outdir = "/data/tmp_files/"
+try:
+    os.mkdir(outdir)
+except:
+    print "Directory already exists"
+
+class OSMMap():
+    def __init__(self):
+        self.values = np.empty((0,2), np.float64)
+        self.coordinates = np.empty((0,2), np.float64)
+
+        self.fig2, self.axes2 = plt.subplots(nrows=1)
+        self.initial = True
+        self.initial_coords = True
+        self.map = None
+        self.fig_map, ax = plt.subplots(ncols=1, nrows=1)
+
+    # ts is just cnt of the array
+    def plot_map(self, ts):
+        if self.initial_coords:
+            # for whole sequences
+            if True:
+                ll_lon = np.min(self.coordinates[:,1]) - 0.0001 
+                ll_lat = np.min(self.coordinates[:,0]) - 0.0001
+
+                ur_lon = np.max(self.coordinates[:,1]) + 0.0001
+                ur_lat = np.max(self.coordinates[:,0]) + 0.0001
+
+                #47.623333, 3.306532
+                #ll_lat = 47.623333
+                #ll_lon = 3.306532
+
+                # 48.043356, 18.272896
+                #ur_lat = 52.604856
+                #ur_lon = 18.272896
+            else:
+                # for area around current position
+                ll_lon = self.coordinates[ts,1] - 0.001
+                ll_lat = self.coordinates[ts,0] - 0.001
+                ur_lon = self.coordinates[ts,1] + 0.001
+                ur_lat = self.coordinates[ts,0] + 0.001
+            
+            # get map
+            try:
+                print "Getting map"
+                self.map = smopy.Map((ll_lat, ll_lon, ur_lat, ur_lon), z=6)
+                x,y = self.map.to_pixels(self.coordinates[:,0], self.coordinates[:,1])
+
+                self.fig_map, ax = plt.subplots(ncols=1, nrows=1)
+                ax = self.map.show_mpl(ax=ax)
+                ax.plot(x,y,'go', ms=2, mew=0)
+
+                cc = self.map.to_pixels(self.coordinates[ts,0], self.coordinates[ts,1])
+                self.current_coord = ax.plot(cc[0], cc[1], 'ro', ms=10)
+
+                # only if we do not refresh the map
+                self.initial_coords = False
+            except Exception,e:
+                print "Exception in plot_map: ", e
+        else:
+            try:
+                self.current_coord[0].set_xdata(self.map.to_pixels(self.coordinates[ts,0], self.coordinates[ts,1])[0])
+                self.current_coord[0].set_ydata(self.map.to_pixels(self.coordinates[ts,0], self.coordinates[ts,1])[1])
+            except Exception, e:
+                print e
+
+        return fig2data(self.fig_map)
 
 class BalticMap():
     def __init__(self):
@@ -36,10 +108,10 @@ class BalticMap():
         self.map = Basemap(
                 #49.267693, 8.406459 # medersche
                 #50.166281, 8.659168 # frankfurt mellsig
-                urcrnrlat = 50.7,
-                urcrnrlon = 9.1,
-                llcrnrlat = 49.3,
-                llcrnrlon = 8.1,
+                urcrnrlat = 51.7,
+                urcrnrlon = 10.1,
+                llcrnrlat = 48.3,
+                llcrnrlon = 6.1,
                     #resolution='h', 
                     resolution='c',
                     area_thresh=5000,
@@ -151,6 +223,7 @@ class BalticMap():
             self.fig.canvas.print_png(outfile)
 
 bm = BalticMap()
+om = OSMMap()
 
 # get coordinates from database
 dbi = DBInterface.DBInterface()
@@ -159,31 +232,58 @@ sequences = dbi.get_all_sequences()
 frames = []
 
 coordt = []
+np_coords = np.empty((0,2), np.float64)
 
 kms = 0.
 old_coords = None
 
 for s in sequences[:]:
-    if s.id == 46:
+    if s.id == 51:
         print "Got sequence w/ id: ", s.id
-        for i,f in enumerate(dbi.get_frames(s.id)[:]):
+        sframes = dbi.get_frames(s.id)
+        # first run, get all coords
+        last = []
+        for i,f in enumerate(sframes):
+            print "%d/%d" % (i, len(sframes))
             coordt.append([f.lat, f.lon])
-            #print "Trying to load orig imae: ", "~/Desktop/camera_data/"+ f.img_uri
-            img = cv2.imread("/home/weis/Desktop/camera_data/" + f.img_uri)
+            if f.lat == None or f.lon == None:
+                print "lat or lon was None !"
+                np_coords = np.append(np_coords, np.array([[last[0], last[1]]]), axis=0)
+            else:
+                last = [f.lat, f.lon]
+                np_coords = np.append(np_coords, np.array([[f.lat, f.lon]]), axis=0)
+        om.coordinates = np_coords
+
+
+        # second run, do gauge, images, km calculations, map
+        for i,f in enumerate(sframes):
+            img = cv2.imread("/media/weis/Transcend/Testfahrt/" + f.img_uri)
+
+            cv2.imwrite(outdir + "/map_image_%08d.png" % i, cv2.cvtColor(om.plot_map(i), cv2.COLOR_BGR2RGB))
 
             if old_coords == None:
-                old_coords = [f.lat, f.lon]
+                if f.lat != None and f.lon != None:
+                    old_coords = [f.lat, f.lon]
             else:
-                dx = 71.5 * (f.lon - old_coords[1])
-                dy = 111.3 * (f.lat - old_coords[0])
-                old_coords = [f.lat, f.lon]
-                kms += math.sqrt(dx * dx + dy * dy)
-                cv2.putText(img,"%.2f km" % (kms), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+                if f.lat != None and f.lon != None:
+                    dx = 71.5 * (f.lon - old_coords[1])
+                    dy = 111.3 * (f.lat - old_coords[0])
+                    old_coords = [f.lat, f.lon]
+                    kms += math.sqrt(dx * dx + dy * dy)
+                    #t_gps = f.ts_gps
+                    #t_str = datetime.datetime.fromtimestamp(int(t_gps)).strftime('%d.%m.%Y %H:%M:%S')
+                    #cv2.putText(img,"%s - %.2f km" % (t_str, kms), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
 
-            speedgauge = bm.g.paint(int((f.speed)*60*60)/1000)
-            cv2.imwrite("./tmp_files/gauge_%08d.png" %i, speedgauge)
+            speedgauge = None
+            if f.speed != None:
+                speedgauge = bm.g.paint(int((f.speed)*60*60)/1000)
+            else:
+                speedgauge = bm.g.paint(int((0)*60*60)/1000)
 
-            cv2.imwrite("./tmp_files/img_%08d.png" % i, img)
-            bm.show_coord_train(coordt, i)
+            gaugename = outdir + "/gauge_%08d.png" %i
+            #print gaugename
+            cv2.imwrite(gaugename, speedgauge)
+            cv2.imwrite(outdir + "/img_%08d.png" %i, img)
+            #bm.show_coord_train(coordt, i)
 
 print "Got %d frames !" % (len(frames))
